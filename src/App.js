02253df28acaa2599ed814a655025cf5d6a4286b
Sambhav Jain <55810174+sambhav2448@users.noreply.github.com>
2020-10-03 21:45:05 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import Signup from "./components/Auth/SignUp";
+import Signin from "./components/Auth/SignIn";
 import Dashboard from "./components/Dashboard/Dashboard";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route path="/signin">
+            <Signin />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
